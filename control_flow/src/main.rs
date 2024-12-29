ce5a301fe52e9e63948308be210da05b859ba174
ethan-stone <ethan.stone9352@gmail.com>
2024-12-28 20:15:11 -0500

    add while loop

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -19,5 +19,15 @@ fn main() {
         }
     };
 
-    println!("The result is: {result}")
+    println!("The result is: {result}");
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF");
 }
