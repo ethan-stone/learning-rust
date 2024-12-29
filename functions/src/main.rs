03d1c333af96af542832e4902935e2a2be4e23b4
ethan-stone <ethan.stone9352@gmail.com>
2024-12-28 20:02:51 -0500

    function that returns something

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,6 +2,10 @@ fn main() {
     another_function(5);
 
     print_labeled_measurement(5, 'h');
+
+    let n = plus_two(3);
+
+    println!("The value of n is: {n}");
 }
 
 fn another_function(x: i32) {
@@ -10,4 +14,8 @@ fn another_function(x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}")
+}
+
+fn plus_two(n: i32) -> i32 {
+    n + 2
 }
