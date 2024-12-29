7dcfabc0aabc49a4e592ba7f2505f3a2b4620dfc
ethan-stone <ethan.stone9352@gmail.com>
2024-12-29 15:34:35 -0500

    example of not being able to mutate a borrowed reference

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -4,8 +4,14 @@ fn main() {
     let length = calculate_length(&s);
 
     println!("The length of {s} is {length}");
+
+    change(&s);
 }
 
 fn calculate_length(s: &String) -> usize {
     return s.len();
+}
+
+fn change(s: &String) {
+    s.push_str(", world!");
 }
