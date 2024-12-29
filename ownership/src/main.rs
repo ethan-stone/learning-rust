ac80a90e586e31a63dca59b1dd9e3d3600f53332
ethan-stone <ethan.stone9352@gmail.com>
2024-12-29 15:18:52 -0500

    example of passing a value into a function means the function now "owns" that value and can not be used after that function call

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
-    let s1 = String::from("hello");
+    let s = String::from("hello");
 
-    let s2 = s1.clone();
+    takes_ownership(s);
 
-    println!("{s1}, world!");
+    println!("{s}");
+
+    let x = 5;
+
+    makes_copy(x);
+
+    println!("{x}")
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{some_string}");
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{some_integer}");
 }
