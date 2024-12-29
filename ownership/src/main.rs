adba693254222fa30e3c7d2f1e4e2005d599a42c
ethan-stone <ethan.stone9352@gmail.com>
2024-12-29 15:31:53 -0500

    basic example of references

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,22 +1,11 @@
 fn main() {
-    // The string made in gives_ownership is moved into s1
-    let s1 = gives_ownership();
+    let s = String::from("hello");
 
-    // s2 comes into scope
-    let s2 = String::from("hello");
+    let length = calculate_length(&s);
 
-    // s2 is passed into takes_and_gives_back, so within takes_and_gives_gack
-    // it has ownership. But then it returns the same variable
-    // so ownership is returned to this scope in s3.
-    let s3 = takes_and_gives_back(s2);
+    println!("The length of {s} is {length}");
 }
 
-fn gives_ownership() -> String {
-    let some_string = String::from("yours");
-
-    return some_string;
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-    return a_string;
+fn calculate_length(s: &String) -> usize {
+    return s.len();
 }
