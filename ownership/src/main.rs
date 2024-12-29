10310d9809a8ea789656fa8d660ec6fc6f55129a
ethan-stone <ethan.stone9352@gmail.com>
2024-12-29 15:30:18 -0500

    example of functions giving and taking ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,21 +1,22 @@
 fn main() {
-    let s = String::from("hello");
+    // The string made in gives_ownership is moved into s1
+    let s1 = gives_ownership();
 
-    takes_ownership(s);
+    // s2 comes into scope
+    let s2 = String::from("hello");
 
-    println!("{s}");
-
-    let x = 5;
-
-    makes_copy(x);
-
-    println!("{x}")
+    // s2 is passed into takes_and_gives_back, so within takes_and_gives_gack
+    // it has ownership. But then it returns the same variable
+    // so ownership is returned to this scope in s3.
+    let s3 = takes_and_gives_back(s2);
 }
 
-fn takes_ownership(some_string: String) {
-    println!("{some_string}");
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+
+    return some_string;
 }
 
-fn makes_copy(some_integer: i32) {
-    println!("{some_integer}");
+fn takes_and_gives_back(a_string: String) -> String {
+    return a_string;
 }
