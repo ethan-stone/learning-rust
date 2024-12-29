b4c0991f8ee0b73a73a0b56950a04bc6b4a1a7f5
ethan-stone <ethan.stone9352@gmail.com>
2024-12-28 20:10:14 -0500

    else if statement

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
     let number = 3;
 
-    if number < 5 {
-        println!("condition was true")
+    if number < 1 {
+        println!("number is less than 1")
+    } else if number >= 1 && number < 3 {
+        println!("number is greater than or equal to 1 and less than 3")
     } else {
-        println!("condition was false")
+        println!("number is greater than or equal to 3")
     }
 }
