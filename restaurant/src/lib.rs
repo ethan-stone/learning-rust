3bfe0d7a80e0aa2f28232dd59fff000d378bf718
ethan-stone <ethan.stone9352@gmail.com>
2025-01-01 20:11:45 -0500

    make stuff public with structs and enums

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -14,6 +14,25 @@ mod front_of_house {
 fn deliver_order() {}
 
 mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        season_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            return Breakfast {
+                toast: String::from(toast),
+                season_fruit: String::from("peaches"),
+            };
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         super::deliver_order();
@@ -23,9 +42,16 @@ mod back_of_house {
 }
 
 pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    let mut meal = back_of_house::Breakfast::summer("Rye");
 
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
+    meal.toast = String::from("Wheat");
+
+    println!("I'd like {} toast please.", meal.toast);
+
+    // this next line won't compile because season_fruit is private
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    // all values of an enum are public if the enum is public
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
