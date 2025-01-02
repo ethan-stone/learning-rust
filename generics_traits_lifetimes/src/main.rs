ac8d34fdb7a1c56334df0e481270347e7aa336ce
ethan-stone <ethan.stone9352@gmail.com>
2025-01-01 22:23:30 -0500

    using generics defined in impl and in the method declaration

--- generics_traits_lifetimes/src/main.rs
+++ generics_traits_lifetimes/src/main.rs
@@ -1,23 +1,22 @@
-fn largest<T>(list: &[T]) -> &T {
-    let mut largest = &list[0];
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
 
-    for item in list {
-        if item > largest {
-            largest = item;
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
         }
     }
-
-    largest
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
 
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
+    let p3 = p1.mixup(p2);
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {result}");
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
