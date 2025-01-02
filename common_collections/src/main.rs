de752233359ce9ee481d7ca7eb952164e32c3d9d
ethan-stone <ethan.stone9352@gmail.com>
2025-01-01 20:53:59 -0500

    iterating on strings

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     vectors();
+    strings()
 }
 
 fn vectors() {
@@ -7,5 +8,23 @@ fn vectors() {
 
     for i in &v {
         println!("{i}");
+    }
+}
+
+fn strings() {
+    let mut s1 = String::from("hello, ");
+    let s2 = "world!";
+
+    s1.push_str(s2);
+
+    println!("{s1}");
+    println!("{s2}");
+
+    for b in s1.bytes() {
+        println!("{b}");
+    }
+
+    for c in s1.chars() {
+        println!("{c}")
     }
 }
