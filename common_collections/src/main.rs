c465ac9efd3853d2282199d51de887fe13364a6c
ethan-stone <ethan.stone9352@gmail.com>
2025-01-01 20:58:17 -0500

    example of making a hashmap, adding some values and getting a value

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -28,3 +28,15 @@ fn strings() {
         println!("{c}")
     }
 }
+
+use std::collections::HashMap;
+
+fn hash_maps() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+}
