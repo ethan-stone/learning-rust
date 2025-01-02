cfadb0f392a7468d941e479c8d73e4305854b163
ethan-stone <ethan.stone9352@gmail.com>
2025-01-01 20:59:27 -0500

    example of iterating over a hashmap

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -38,8 +38,7 @@ fn hash_maps() {
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
 
-    let team_name = String::from("Blue");
-    let score = scores.get(&team_name).copied().unwrap_or(0);
-
-    println!("score: {score}");
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
 }
