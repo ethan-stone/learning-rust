ab0505380b83ee8d15f64818e6d986116c5c5fd7
ethan-stone <ethan.stone9352@gmail.com>
2025-01-01 20:38:33 -0500

    accessing a vector using indexing

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    vectors();
+}
+
+fn vectors() {
+    let mut v: Vec<i32> = Vec::new();
+
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    let first = &v[0];
+
+    println!("First element is {first}");
 }
