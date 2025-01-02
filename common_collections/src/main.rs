01c262f89f862666b2941a5b97a60d8a541371e9
ethan-stone <ethan.stone9352@gmail.com>
2025-01-01 20:42:01 -0500

    iterating over vectors

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -3,19 +3,9 @@ fn main() {
 }
 
 fn vectors() {
-    let mut v: Vec<i32> = Vec::new();
+    let v: Vec<i32> = vec![100, 32, 57];
 
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    v.push(8);
-
-    // This is an immutable references
-    let first = &v[0];
-
-    // Here we mutate the array, thus borrowing a mutable references
-    v.push(6);
-
-    // so we can't use "first" here by rules of borrowing
-    println!("The first elemetn is: {first}");
+    for i in &v {
+        println!("{i}");
+    }
 }
