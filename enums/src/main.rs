660ef5a2bbe6e2cd4f10d0711cda90639af99b93
ethan-stone <ethan.stone9352@gmail.com>
2025-01-01 19:31:28 -0500

    example of enum with default arm

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
-}
+    let dice_roll = 8;
 
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        Some(i) => Some(i + 1),
+    match dice_roll {
+        3 => {
+            println!("3");
+        }
+        other => {
+            println!("other: {other}")
+        }
     }
 }
