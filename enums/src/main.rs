53b8db5605ce4c77fbc8e162ee07254362184976
ethan-stone <ethan.stone9352@gmail.com>
2025-01-01 19:39:30 -0500

    example of if let statement

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,12 +1,22 @@
 fn main() {
-    let dice_roll = 8;
+    let ac_power_type = PowerType::AC(32);
 
-    match dice_roll {
-        3 => {
-            println!("3");
-        }
-        other => {
-            println!("other: {other}")
-        }
+    if let PowerType::DC(amperage) = ac_power_type {
+        println!("PowerType is DC and amperage is {amperage}");
+    } else {
+        println!("PowerType is not DC");
     }
+
+    let dc_power_type = PowerType::DC(32);
+
+    if let PowerType::AC(amperage) = dc_power_type {
+        println!("PowerType is AC and amperage is {amperage}");
+    } else {
+        println!("PowerType is not AC");
+    }
+}
+
+enum PowerType {
+    AC(u32),
+    DC(u32),
 }
