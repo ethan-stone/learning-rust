90cd8d01f172839867a1a032fa9d53c205d69642
ethan-stone <ethan.stone9352@gmail.com>
2025-01-04 14:34:29 -0500

    testing using assert!

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -1,5 +1,17 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug)]
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        return self.length > other.length && self.width < other.width;
+    }
+}
+
+pub fn add_two(a: usize) -> usize {
+    a + 2
 }
 
 #[cfg(test)]
@@ -7,13 +19,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn exploration() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            length: 10,
+            width: 10,
+        };
+
+        let smaller = Rectangle {
+            length: 5,
+            width: 5,
+        };
+
+        assert!(larger.can_hold(&smaller));
     }
 
     #[test]
-    fn another(){
-        panic!("Make this test fail")
+    fn smaller_can_not_hold_larger() {
+        let larger = Rectangle {
+            length: 10,
+            width: 10,
+        };
+
+        let smaller = Rectangle {
+            length: 5,
+            width: 5,
+        };
+
+        assert!(!smaller.can_hold(&larger));
     }
 }
