b7e3849aae26b6b72e5a999a4f1147726995a0eb
ethan-stone <ethan.stone9352@gmail.com>
2025-01-03 21:54:37 -0500

    example of good usage of generic lifetimes

--- generics_traits_lifetimes/src/main.rs
+++ generics_traits_lifetimes/src/main.rs
@@ -1,58 +1,28 @@
-struct Point<X1, Y1> {
-    x: X1,
-    y: Y1,
-}
-
-impl<X1, Y1> Point<X1, Y1> {
-    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
-        Point {
-            x: self.x,
-            y: other.y,
-        }
-    }
-}
-
-pub mod aggregator;
-
-use aggregator::{Summary, Tweet};
-
-use std::fmt::Display;
-
-struct Pair<T> {
-    x: T,
-    y: T,
-}
-
-impl<T> Pair<T> {
-    fn new(x: T, y: T) -> Self {
-        Self { x, y }
-    }
-}
-
-impl<T: Display + PartialOrd> Pair<T> {
-    fn cmp_display(&self) {
-        if self.x >= self.y {
-            println!("The largest number is x = {}", self.x);
-        } else {
-            println!("The largest number is y = {}", self.y);
-        }
-    }
-}
-
 fn main() {
-    let p1 = Point { x: 5, y: 10.4 };
-    let p2 = Point { x: "Hello", y: 'c' };
+    let string1 = String::from("abcd");
 
-    let p3 = p1.mixup(p2);
+    // This works
+    {
+        let string2 = "xyz";
 
-    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+        let result = longest(string1.as_str(), string2);
 
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
-    };
+        println!("The longest string is {result}");
+    }
+}
 
-    println!("1 new tweet: {}", tweet.summary());
+/**
+ * The 'a is a lifetime generic. Basically 'a represents a lifetime (which is how long a variable can last)
+ *
+ * By using 'a to define x and y, we are saying that the lifetimes of x and y are at least as long as 'a.
+ *
+ * In practice this means that the lifetime of the returned reference is the same as the smaller of the lifetimes
+ * of the values referenced in the function arguments.
+ */
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        return x;
+    } else {
+        return y;
+    }
 }
