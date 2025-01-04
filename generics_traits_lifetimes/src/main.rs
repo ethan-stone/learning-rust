dc655ad8ae6f659d1af42f8e91c88310a9c6c9b8
ethan-stone <ethan.stone9352@gmail.com>
2025-01-03 22:21:06 -0500

    example function that uses generics, traits, and lifetimes

--- generics_traits_lifetimes/src/main.rs
+++ generics_traits_lifetimes/src/main.rs
@@ -1,26 +1,15 @@
-// 'a is a generic lifetime
-struct ImportantExcerpt<'a> {
-    part: &'a str, // the part field will have the lifetime 'a
-}
+use std::fmt::Display;
 
-// Since one of the lifetimes &self, the life time of the return reference is the same as the
-// the struct itself.
-// In other words, the referenced returned by announce_and_return_part can only live as long
-// as the struct is alive.
-impl<'a> ImportantExcerpt<'a> {
-    fn announce_and_return_part(&self, announcement: &str) -> &str {
-        println!("Attention please: {announcement}");
-        return self.part;
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {ann}");
+    if x.len() > y.len() {
+        return x;
+    } else {
+        return y;
     }
 }
 
-fn main() {
-    let novel = String::from("Call me Ishmael. Some years ago...");
-    let first_sentence = novel.split('.').next().unwrap();
-
-    // this particular usage of ImportantExcerpt means that
-    // i can't outlive first_sentence.
-    let i = ImportantExcerpt {
-        part: first_sentence,
-    };
-}
+fn main() {}
