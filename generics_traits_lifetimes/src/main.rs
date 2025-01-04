501b1576aeee480d6c773ad0419c0fb0e85b62d5
ethan-stone <ethan.stone9352@gmail.com>
2025-01-03 22:17:17 -0500

    example of a struct with lifetimes

--- generics_traits_lifetimes/src/main.rs
+++ generics_traits_lifetimes/src/main.rs
@@ -1,35 +1,26 @@
-fn main() {
-    let string1 = String::from("abcd");
-    let result;
-
-    /*
-     * This does not work.
-     * The reason is because the longest function using lifetime generics.
-     * So when we call this function, the lifetime that is chosen for 'a that is in the signature for longest is the shorter
-     * of string1 and string2. That means the lifetime of the returned reference will only live for as long as string2,
-     * which is the inner scope.
-     */
-    {
-        let string2 = String::from("xyz");
-
-        result = longest(string1.as_str(), string2.as_str());
-    };
-
-    println!("The longest string is {result}");
+// 'a is a generic lifetime
+struct ImportantExcerpt<'a> {
+    part: &'a str, // the part field will have the lifetime 'a
 }
 
-/**
- * The 'a is a lifetime generic. Basically 'a represents a lifetime (which is how long a variable can last)
- *
- * By using 'a to define x and y, we are saying that the lifetimes of x and y are at least as long as 'a.
- *
- * In practice this means that the lifetime of the returned reference is the same as the smaller of the lifetimes
- * of the values referenced in the function arguments.
- */
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        return x;
-    } else {
-        return y;
+// Since one of the lifetimes &self, the life time of the return reference is the same as the
+// the struct itself.
+// In other words, the referenced returned by announce_and_return_part can only live as long
+// as the struct is alive.
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {announcement}");
+        return self.part;
     }
+}
+
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().unwrap();
+
+    // this particular usage of ImportantExcerpt means that
+    // i can't outlive first_sentence.
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
