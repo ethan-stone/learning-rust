eb30c863dc89ee668f3e613855b8bd736e99685e
ethan-stone <ethan.stone9352@gmail.com>
2025-01-08 21:54:25 -0500

    use of reference counter (Rc)

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,5 +1,8 @@
 use std::mem::drop;
 use std::ops::{Deref, Drop};
+use std::rc::Rc;
+
+use crate::List::{Cons, Nil};
 
 struct MyBox<T>(T);
 
@@ -27,6 +30,11 @@ impl Drop for CustomSmartPoint {
     }
 }
 
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
 fn main() {
     let m = MyBox::new(String::from("Rust"));
     hello(&m);
@@ -42,6 +50,16 @@ fn main() {
     drop(c);
 
     println!("CustomSmartPointers created.");
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
 
 fn hello(name: &str) {
