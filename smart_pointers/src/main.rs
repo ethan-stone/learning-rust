1ee0b8741472fb4c5db8851717d1b9f2ce58d6e5
ethan-stone <ethan.stone9352@gmail.com>
2025-01-08 21:33:47 -0500

    example of Deref trait with Box

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,10 +1,25 @@
-use crate::List::{Cons, Nil};
+use std::ops::Deref;
 
-fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
 }
 
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
